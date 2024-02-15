d2d22bee39f73bc0b62e3dbccda1f3dbd2534b23
savaliyasahil801 <savaliyasahil801@gmail.com>
2024-02-15 20:53:42 +0530

    1

---
+++ data.js
@@ -0,0 +1,12 @@
+// 20240215104815
+// https://raw.githubusercontent.com/anshvekariya/ansh/main/ansh.js
+
+[
+    {
+      "userid": "1",
+      "user_name": "ansh patel",
+      "user_email": "ansh@gmail.com",
+      "user_address": "USA",
+      "user_status": "1"
+    }
+  ]
