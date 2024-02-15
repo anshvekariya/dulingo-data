25ab87bc4bcc29e37041ed0e7b8c0ad91dfc603a
savaliyasahil801 <savaliyasahil801@gmail.com>
2024-02-15 21:11:44 +0530

    2

--- data.js
+++ data.js
@@ -1,9 +1,25 @@
+// 20240215211059
+// https://raw.githubusercontent.com/anshvekariya/ansh/main/ansh.js
 
 [
     {
       "userid": "1",
-      "user_name": "ansh patel",
-      "user_email": "ansh@gmail.com",
+      "user_name": "ansh ansh ansh",
+      "user_email": "david_frank@gmail.com",
+      "user_address": "Australia",
+      "user_status": "1"
+    },
+    {
+      "userid": "2",
+      "user_name": "john_test",
+      "user_email": "john_test@gmail.com",
+      "user_address": "USA",
+      "user_status": "1"
+    },
+    {
+      "userid": "3",
+      "user_name": "Tess",
+      "user_email": "tmateiko0@gmail.com",
       "user_address": "USA",
       "user_status": "1"
     }
